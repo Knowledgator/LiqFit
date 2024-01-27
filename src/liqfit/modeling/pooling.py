261750ace80f15974abb63390af0a31f6f8f50c5
Hazem <hazemessam199@gmail.com>
2024-01-27 16:59:13 +0200

    fixed pooling axis

--- src/liqfit/modeling/pooling.py
+++ src/liqfit/modeling/pooling.py
@@ -8,22 +8,22 @@ class GlobalMaxPooling1D(nn.Module):
     """Applies Global Max Pooling on the timesteps dimension."""
 
     def forward(self, x: torch.Tensor):
-        return x.amax(-1)
+        return x.amax(dim=1)
 
 
 class FirstTokenPooling1D(nn.Module):
     """Takes the first token's embedding."""
 
     def forward(self, x: torch.Tensor):
-        return x[:, 0]
+        return x[:, 0, :]
 
 
 class LastTokenPooling1D(nn.Module):
     """Takes the last token's embedding."""
-    
+
     def forward(self, x: torch.Tensor):
-        return x[:, -1]
-    
+        return x[:, -1, :]
+
 
 class GlobalAvgPooling1D(nn.Module):
     """Applies Global Average Pooling on the timesteps dimension."""
@@ -38,57 +38,53 @@ class GlobalAvgPooling1D(nn.Module):
             x = x * attention_mask
             return x.sum(1) / attention_mask.sum(1)
         else:
-            return x.mean(dim=-1)
+            return x.mean(dim=1)
 
 
 class GlobalSumPooling1D(nn.Module):
     """Applies Global Sum Pooling on the timesteps dimension."""
 
     def forward(self, x: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
-        
         if attention_mask is not None:
             x = x * attention_mask
-        
-        return x.sum(-1)
+        return x.sum(dim=1)
 
 
 class GlobalRMSPooling1D(nn.Module):
     """Applies Global RMS Pooling on the timesteps dimension."""
 
     def forward(self, x: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
-        
         if attention_mask is not None:
             attention_mask = attention_mask.repeat((1, 1, x.shape[-1])).to(
                 dtype=x.dtype
             )
             x = x * attention_mask
-        
-        return x.pow(2).mean(-1).sqrt()
-        
+            return (x.pow(2).sum(dim=1) / attention_mask.sum(1)).sqrt()
+        else:
+            return x.pow(2).mean(dim=1).sqrt()
+
 
 class GlobalAbsMaxPooling1D(nn.Module):
     """Applies Global Max Pooling of absolute values on the timesteps dimension."""
 
     def forward(self, x: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
-        
         if attention_mask is not None:
             attention_mask = attention_mask.repeat((1, 1, x.shape[-1])).to(
                 dtype=x.dtype
             )
             x = x * attention_mask
-        
-        return x.abs().amax(-1)
+        return x.abs().amax(dim=1)
 
 
 class GlobalAbsAvgPooling1D(nn.Module):
     """Applies Global Average Pooling of absolute values on the timesteps dimension."""
 
     def forward(self, x: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
-        
         if attention_mask is not None:
             attention_mask = attention_mask.repeat((1, 1, x.shape[-1])).to(
                 dtype=x.dtype
             )
-            x = x * attention_mask
-        
-        return x.abs().mean(-1)
+            x = (x * attention_mask).abs()
+            return x.sum(dim=1) / attention_mask.sum(1)
+        else:
+            return x.abs().mean(dim=1)
